c42ea8909eec7c266c733a204d9725ddd825de72
chandraauliatama <chandraaulia4@gmail.com>
2022-02-18 15:37:48 +0700

    create different view for auth and guest

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,32 +10,56 @@
 <body>
     <div class="mx-auto max-w-4xl px-6 grid grid-cols-8 gap-12 mt-16">
         <div class="hidden sm:block col-span-2 border-r border-slate-200 space-y-6">
-            <ul>
-                <li>
-                    <span class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
-                        Chandra
-                    </span>
-                </li>
-                <li>
-                    <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
-                        Feed
-                    </a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
-                        My Books
-                    </a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
-                        Log Out
-                    </a>
-                </li>
-            </ul>
+            @auth
+                <ul>
+                    <li>
+                        <span class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
+                            {{ auth()->user()->name }}
+                        </span>
+                    </li>
+                    <li>
+                        <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
+                            Feed
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
+                            My Books
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
+                            Log Out
+                        </a>
+                    </li>
+                </ul>
+            @endauth
+
+            @guest
+                <ul>
+                    <li>
+                        <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
+                            Home
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
+                            Login
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="font-bold text-lg text-slate-600 hover:text-slate-800 block py-1">
+                            Register
+                        </a>
+                    </li>
+                </ul>
+            @endguest
         </div>
         <div class="col-span-6">
             @isset($header)
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,13 @@
 <x-layouts.app>
-    <x-slot name="header">
-        Home
-    </x-slot>
-    <div class="mt-8">
-        Home
-    </div>
+    @guest
+        <x-slot name="header">
+            ReadWithFriends
+        </x-slot>
+        <div class="mt-8">
+            <p>See what your friend reads and update what's your reading .</p>
+        </div>
+        <div class="mt-8">
+            <p>Sign up to get started.</p>
+        </div>
+    @endguest
 </x-layouts.app>
