31f672870192e827f1f77dcd68a56efe894284b9
chandraauliatama <chandraaulia4@gmail.com>
2022-03-15 19:33:36 +0700

    contoh upload ke online

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', HomeController::class);
+Route::get('/', HomeController::class)->name('home');
 Route::resource('books', BookController::class)->middleware('auth');
